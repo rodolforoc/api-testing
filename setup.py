27b01b34550d59d7ea8e6239c4fed6966598235c
rodolforoc <rodolforoc97@gmail.com>
2021-05-05 16:04:44 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='api-testing',
+      version='1.0',
+      description="Practice API Testing",
+      author='Rodolfo Caetano',
+      author_email='rodolforoc97@gmail.com',
+      url='',
+      packages=find_packages()
+      )
